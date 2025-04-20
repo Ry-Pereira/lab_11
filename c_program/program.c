819679af0dcb88dbba1627b9469bef4781a50209
Ryan <145312422+Ry-Pereira@users.noreply.github.com>
2025-04-19 21:20:36 -0500

    Finished

---
+++ c_program/program.c
@@ -0,0 +1,22 @@
+//Program Name: Lab 11: Hello World
+//Brief Description: This program prints hello world to the terminal
+//Input: No input
+//Ouput: The ouput will be hello world printed to the terminal.
+//All collaborators: None
+//Other Sources:No sources
+//Author: Ryan Antony Pereira
+//Creation Date: 4/19/2025
+//Last Modified Date: 4/19/2025
+
+
+//Included the header file library that enables input and output functionality
+#include <stdio.h>
+
+//The main function for the program
+int main()
+{
+	//Prints hello world string to terminal
+    printf("Hello World!");
+	//Returns 0 to indcate that the program has ended successfully
+    return 0;
+}
